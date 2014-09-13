cae2597b25020d318a6bccdd1137f0662ba92d2a
Samuel J. Knox <evorlor@gmail.com>
2014-09-13 19:44:44 -0400

    Added scene with Pathfinding and Player Selectability Removed Gravity

---
+++ Assets/ClickableMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PolyNavAgent))]
+[RequireComponent(typeof(Selectable))]
+public class ClickableMovement : MonoBehaviour
+{
+    public PolyNavAgent agent
+    {
+        get
+        {
+            return GetComponent<PolyNavAgent>();
+        }
+    }
+    public Selectable Selectable
+    {
+        get
+        {
+            return GetComponent<Selectable>();
+        }
+    }
+
+    void Update()
+    {
+        UpdateDestination();
+    }
+
+    private void UpdateDestination()
+    {
+        bool shouldChangeDestination = Selectable.Selected && Input.GetMouseButtonDown(1);
+        if (shouldChangeDestination)
+        {
+            Vector2 newDestination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            agent.SetDestination(newDestination);
+        }
+    }
+}
---
+++ Assets/Selectable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Selectable : MonoBehaviour
+{
+    [Tooltip("Whether or not the gameObject is currently under user control")]
+    public bool Selected = false;
+
+    void OnMouseDown()
+    {
+        SetSelected(!Selected);
+    }
+
+    private void SetSelected(bool selected)
+    {
+        Selected = selected;
+        if (Selected)
+        {
+            gameObject.renderer.material.color = Color.green;
+        }
+        else
+        {
+            gameObject.renderer.material.color = Color.white;
+        }
+    }
+}
