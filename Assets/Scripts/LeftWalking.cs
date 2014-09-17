1dd4635e04d025e026dd1d709ebf0be3d6e0181b
Samuel J. Knox <evorlor@gmail.com>
2014-09-16 22:46:22 -0400

    The Pacing AI

---
+++ Assets/Scripts/LeftWalking.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PolyNavAgent))]
+public class LeftWalking : MonoBehaviour
+{
+
+    public PolyNavAgent agent
+    {
+        get
+        {
+            return GetComponent<PolyNavAgent>();
+        }
+    }
+    private bool applicationClosing;
+    // Use this for initialization
+    void Start()
+    {
+        agent.SetDestination(new Vector2(-9, -1));
+    }
+
+    void OnApplicationQuit()
+    {
+        applicationClosing = true;
+    }
+    void OnDestinationReached()
+    {
+        Destroy(gameObject.GetComponent<LeftWalking>());
+    }
+    void OnDestroy()
+    {
+        if (applicationClosing)
+        {
+            return;
+        }
+
+        gameObject.AddComponent<RightWalking>();
+    }
+}
