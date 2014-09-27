e27b92b7382b43dd356a7cfeb143223cd1177d4e
Samuel J. Knox <evorlor@gmail.com>
2014-09-27 13:53:46 -0400

    Added Vision and Hearing AI Chases player until player escapes Prefabbed Both the AI and the Player Still need to fix alpha clash with background

--- Assets/Scenes/readme.txt
+++ Assets/Scenes/readme.txt
@@ -1,1 +1,3 @@
 This folder is for the Scenes.  All scenes, and only scenes, must go in this folder.
+
+Only ever have one Scene checked out at a time.  This means, only one BRANCH can work on a scene at a time.  Also, only one PERSON can work on a scene at a time.
---
+++ Assets/Scripts/ChasedByGuards.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChasedByGuards : MonoBehaviour
+{
+    void Start()
+    {
+        Light2D.RegisterEventListener(LightEventListenerType.OnStay, OnLightStay);
+    }
+
+    void OnDestroy()
+    {
+        Light2D.UnregisterEventListener(LightEventListenerType.OnStay, OnLightStay);
+    }
+
+    void OnLightStay(Light2D _lightObject, GameObject _gameObject)
+    {
+        if (_gameObject.GetInstanceID() == gameObject.GetInstanceID())
+        {
+            Transform lightSource = _lightObject.transform.parent;
+            if (lightSource.tag == "Guard")
+            {
+                BeChasedByGuard(lightSource);
+            }
+        }
+    }
+
+    private void BeChasedByGuard(Transform guard)
+    {
+        PolyNavAgent agent = guard.GetComponent<PolyNavAgent>();
+        if (agent != null)
+        {
+            agent.SetDestination(gameObject.transform.position);
+        }
+    }
+}
