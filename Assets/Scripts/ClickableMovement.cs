1ea7d0e502dd1acddac4f20312f4981f6adfbfa0
jackmacattack <jackmacattack92@gmail.com>
2014-10-03 12:48:22 -0400

    Set up advance clicking. Resolved #16

--- Assets/Scripts/ClickableMovement.cs
+++ Assets/Scripts/ClickableMovement.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+public class Waypoint
+{
+    public Vector3 Pos;
+    public bool Sneak;
+
+    public Waypoint(Vector3 pos_, bool sneak_)
+    {
+        Pos = pos_;
+        Sneak = sneak_;
+    }
+}
+
 [RequireComponent(typeof(PolyNavAgent))]
 [RequireComponent(typeof(Selectable))]
+[RequireComponent(typeof(Sneaking))]
 public class ClickableMovement : MonoBehaviour
 {
     [Tooltip("Ability to Shift-Right-Click to set Waypoints enabled")]
     public bool Waypoints = true;
 
-    private Vector3 pointToAdd;
+    private Waypoint pointToAdd;
 
-    public void AddPoint(Vector3 vec)
+    public void AddPoint(Waypoint vec)
     {
         pointToAdd = vec;
     }
@@ -30,7 +43,7 @@ public class ClickableMovement : MonoBehaviour
         }
     }
 
-    private Queue<Vector2> waypoints = new Queue<Vector2>();
+    private Queue<Waypoint> waypoints = new Queue<Waypoint>();
 
     void Update()
     {
@@ -39,7 +52,7 @@ public class ClickableMovement : MonoBehaviour
 
     private void UpdateDestinations()
     {
-        bool shouldAddDestination = Selectable.Selected && pointToAdd != Vector3.zero;//Input.GetMouseButtonDown(1);
+        bool shouldAddDestination = Selectable.Selected && pointToAdd != null;//Input.GetMouseButtonDown(1);
         if (shouldAddDestination)
         {
             bool shouldClearWaypoints = !Input.GetKey(KeyCode.LeftShift) || !Waypoints;
@@ -47,14 +60,18 @@ public class ClickableMovement : MonoBehaviour
             {
                 waypoints.Clear();
             }
-            Vector2 destination = Camera.main.ScreenToWorldPoint(pointToAdd);//Input.mousePosition);
-            pointToAdd = Vector3.zero;
+            pointToAdd.Pos = Camera.main.ScreenToWorldPoint(pointToAdd.Pos);//Input.mousePosition);
 
-            waypoints.Enqueue(destination);
+            waypoints.Enqueue(pointToAdd);
             if (waypoints.Count == 1)
             {
-                agent.SetDestination(waypoints.Peek());
+                Sneaking sneak = GetComponent<Sneaking>();
+                Waypoint next = waypoints.Peek();
+                sneak.SetSneaking(next.Sneak);
+                agent.SetDestination(next.Pos);
             }
+
+            pointToAdd = null;
         }
     }
 
@@ -62,7 +79,10 @@ public class ClickableMovement : MonoBehaviour
     {
         if (waypoints.Count > 0)
         {
-            agent.SetDestination(waypoints.Dequeue());
+            Sneaking sneak = GetComponent<Sneaking>();
+            Waypoint next = waypoints.Dequeue();
+            sneak.SetSneaking(next.Sneak);
+            agent.SetDestination(next.Pos);
         }
     }
 }
