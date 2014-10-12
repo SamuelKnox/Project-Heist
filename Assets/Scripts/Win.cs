b65ce1e2f792b48018a70ad530906fe1a44a762f
jackmacattack <jackmacattack92@gmail.com>
2014-10-12 18:26:08 -0400

    Resolved #23

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Win : MonoBehaviour {
+
+    public bool victory;
+
+	// Use this for initialization
+	void Start () {
+        victory = false;
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if(other.gameObject.GetComponent<Selectable>() != null)
+            victory = true;
+    }
+
+    void OnGUI()
+    {
+        if (victory)
+        {
+            GUI.TextField(new Rect(50,50,200,50), "You're Winner!");
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
